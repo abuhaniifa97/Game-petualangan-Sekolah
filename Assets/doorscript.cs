9dd0845d484aa2c886fcaa577553652f6b88b687
abuhaniifa097 <abuhaniifa097@gmail.com>
2019-05-31 21:20:11 +0700

    first commit

---
+++ Assets/doorscript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorscript : MonoBehaviour {
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim =GetComponent<Animator>();
+
+		
+	}
+	void OnTriggerEnter(Collider other)
+{
+anim.SetTrigger("opendoor");
+}
+void OnTriggerExit(Collider other)
+{
+
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/minimap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimap : MonoBehaviour {
+	public Transform player;
+
+	// Use this for initialization
+	void LateUpdate()
+	{
+		Vector3 newPosition = player.position;
+		newPosition.y = transform.position.y;
+		transform.position = newPosition;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
